055b46aa8b653ce3e4676815adba3503415d2622
takumi-saito <takumi.saitoh.1992@gmail.com>
2020-10-01 04:07:57 +0900

    Initial Commit

---
+++ CalcApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalcApp
+//
+//  Created by Takumi Saito on 2020/10/01.
+//  Copyright © 2020 Takumi Saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
